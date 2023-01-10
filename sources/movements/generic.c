f3aa3d2e29ad4510a199e36ef51eceaf9102fd38
Douglas Fanucchi <douglasfanucchi@gmail.com>
2023-01-10 17:23:51 -0300

    implements generic swap

--- sources/movements/generic.c
+++ sources/movements/generic.c
@@ -6,7 +6,7 @@
 /*   By: dfanucch <dfanucch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 15:57:29 by dfanucch          #+#    #+#             */
-/*   Updated: 2023/01/10 17:14:04 by dfanucch         ###   ########.fr       */
+/*   Updated: 2023/01/10 17:21:58 by dfanucch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,4 +46,15 @@ void	ft_push(int *origin, int *dest, int *size_origin, int *size_dest)
 	ft_memmove(dest + 1, dest, *size_dest * sizeof(int));
 	dest[0] = top;
 	*size_dest += 1;
+}
+
+void	ft_swap(int *stack, int size)
+{
+	int	tmp;
+
+	if (size < 2)
+		return ;
+	tmp = stack[0];
+	stack[0] = stack[1];
+	stack[1] = tmp;
 }
