5d091f4024f6dc5761e367189cc42a3fdc5b282c
Douglas Fanucchi <douglasfanucchi@gmail.com>
2023-01-10 17:47:55 -0300

    refactors generic push prototype

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: dfanucch <dfanucch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 15:35:24 by dfanucch          #+#    #+#             */
-/*   Updated: 2023/01/10 17:32:57 by dfanucch         ###   ########.fr       */
+/*   Updated: 2023/01/10 17:44:25 by dfanucch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 void	ft_rotate(int *stack, int size);
 void	ft_reverse_rotate(int *stack, int size);
-void	ft_push(int *stack_a, int *stack_b, int *size_a, int *size_b);
+void	ft_push(int *origin, int *dest, int *origin_size, int *dest_size);
 void	ft_swap(int *stack, int size);
 void	ft_swap_both(int *stack_a, int *stack_b, int size_a, int size_b);
 
