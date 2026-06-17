39260d71f1343d928ca07aa0d513cb69e849cf57
Douglas Fanucchi <douglasfanucchi@gmail.com>
2026-06-17 17:30:58 -0300

    tests: ft_stack_push should push an element into an empty stack

--- tests/ft_stack_test.cc
+++ tests/ft_stack_test.cc
@@ -12,3 +12,18 @@ TEST(ft_stack, itShouldInitializeAStack) {
 	ASSERT_EQ(0, stack.size);
 	ASSERT_EQ(nullptr, stack.head);
 }
+
+TEST(ft_stack, itShouldPushAnElementIntoAnEmptyStack) {
+	t_stack stack;
+	stack.size = 0;
+	stack.head = NULL;
+
+	int size = ft_stack_push(&stack, 42);
+
+	ASSERT_EQ(1, size);
+	ASSERT_EQ(1, stack.size);
+	ASSERT_NE(nullptr, stack.head);
+	ASSERT_EQ(42, stack.head->n);
+
+	free(stack.head);
+}
