6102130bb245f46f6d98ca0b1a78968820f21434
Douglas Fanucchi <douglasfanucchi@gmail.com>
2026-06-18 19:00:40 -0300

    tests: ft_exec_operation should execute "push a" on stack a

--- tests/push_swap_test.cc
+++ tests/push_swap_test.cc
@@ -25,6 +25,13 @@ static t_operations	ft_swap_ss(t_stack *stack_a, t_stack *stack_b)
 	return (ss);
 }
 
+static t_operations	ft_push_a(t_stack *stack_a, t_stack *stack_b)
+{
+	(void)stack_a;
+	(void)stack_b;
+	return (pa);
+}
+
 TEST(push_swap, itShouldExecuteSwapOperationOnStackA) {
 	t_state	state;
 	t_stack	stack_a;
@@ -72,6 +79,23 @@ TEST(push_swap, itShouldExecuteSwapOperationOnBothStacks) {
 	ASSERT_EQ(1, state.total_ops);
 	ASSERT_NE(nullptr, state.ops);
 	ASSERT_STREQ("ss", (char *)state.ops->content);
+
+	ft_lstclear(&state.ops, free);
+}
+
+TEST(push_swap, itShouldExecutePushOperationOnStackA) {
+	t_state	state;
+	t_stack	stack_a, stack_b;
+	state.count_ops[pa] = 0;
+	state.total_ops = 0;
+	state.ops = NULL;
+
+	ft_exec_operation(&state, &stack_a, &stack_b, ft_push_a);
+	
+	ASSERT_EQ(1, state.count_ops[pa]);
+	ASSERT_EQ(1, state.total_ops);
+	ASSERT_NE(nullptr, state.ops);
+	ASSERT_STREQ("pa", (char *)state.ops->content);
 
 	ft_lstclear(&state.ops, free);
 }
