0e4a5dc309d5361c96a06d76b2d64696c9adb352
Douglas Fanucchi <douglasfanucchi@gmail.com>
2026-06-18 16:29:33 -0300

    tests: ft_stack_reverse_rotate should reverse rotate a stack with more than one element

--- tests/ft_stack_operations_test.cc
+++ tests/ft_stack_operations_test.cc
@@ -106,3 +106,26 @@ TEST(ft_stack, itShouldReverseRotateAStackWithOneElement) {
 	ASSERT_EQ(stack.head, stack.head->next);
 	ASSERT_EQ(stack.head, stack.head->prev);
 }
+
+TEST(ft_stack, itShouldReverseRotateAStackWithMoreThanOneElement) {
+	t_stack stack;
+	t_dlist node_1, node_2, node_3;
+	node_1.n = 42;
+	node_1.next = &node_2;
+	node_1.prev = &node_3;
+	node_2.n = 43;
+	node_2.prev = &node_1;
+	node_2.next = &node_3;
+	node_3.n = 44;
+	node_3.prev = &node_2;
+	node_3.next = &node_1;
+	stack.size = 3;
+	stack.head = &node_1;
+
+	ft_stack_reverse_rotate(&stack);
+
+	ASSERT_EQ(44, stack.head->n);
+	ASSERT_EQ(&node_3, stack.head);
+	ASSERT_EQ(&node_2, stack.head->prev);
+	ASSERT_EQ(&node_1, stack.head->next);
+}
