a8f605a51fa7f13854f88f2d0463fcfa493a5b0d
Douglas Fanucchi <douglasfanucchi@gmail.com>
2026-06-22 17:54:40 -0300

    feat(ft_state): it implements ft_state_update

--- ft_state.c
+++ ft_state.c
@@ -24,3 +24,20 @@ int ft_state_get_operation_count(const t_state *state, t_operations op)
 {
     return state->count_ops[op];
 }
+
+t_bool  ft_state_update(t_state *state, t_operations op)
+{
+    char    *label;
+    t_list  *node;
+
+    label = ft_get_operation_label(op);
+    if (!label)
+        return (FALSE);
+    node = ft_lstnew(label);
+    if (!node)
+        return (FALSE);
+    ft_lstadd_back(&state->ops, node);
+    state->count_ops[op]++;
+    state->total_ops++;
+    return (TRUE);
+}
--- push_swap.h
+++ push_swap.h
@@ -25,6 +25,8 @@ typedef struct s_stack {
 	t_dlist	*head;
 }	t_stack;
 
+typedef unsigned char t_bool;
+
 typedef enum e_operations {
 	sa,
 	sb,
@@ -63,5 +65,6 @@ char	*ft_get_operation_label(t_operations op);
 void	ft_state_init(t_state *state);
 int		ft_state_print(const t_state *state);
 int		ft_state_get_operation_count(const t_state *state, t_operations op);
+t_bool	ft_state_update(t_state *state, t_operations op);
 
 #endif
