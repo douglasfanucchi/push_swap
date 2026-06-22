ad9a2b357e5ead13ee838ae33a387b2a134cd4f5
Douglas Fanucchi <douglasfanucchi@gmail.com>
2026-06-22 15:43:31 -0300

    tests(ft_state): it should print state operations

--- tests/ft_state_test.cc
+++ tests/ft_state_test.cc
@@ -2,6 +2,8 @@
 
 extern "C" {
     #include "push_swap.h"
+    #include <fcntl.h>
+    #include <unistd.h>
 }
 
 TEST(ft_state, itShouldInitializeState) {
@@ -22,4 +24,27 @@ TEST(ft_state, itShouldInitializeState) {
     ASSERT_EQ(0, state.count_ops[rra]);
     ASSERT_EQ(0, state.count_ops[rrb]);
     ASSERT_EQ(0, state.count_ops[rrr]);
+}
+
+TEST(ft_state, itShouldPrintOperations) {
+    t_state state;
+    t_list node_1, node_2;
+    state.ops = &node_1;
+    node_1.content = (void *)"sa";
+    node_1.next = &node_2;
+    node_2.content = (void *)"pb";
+    node_2.next = NULL;
+
+    int stdout = dup(1), fd[2];
+    int piped = pipe2(fd, O_NONBLOCK);
+    dup2(fd[1], 1);
+
+    int result = ft_state_print(&state);
+    dup2(stdout, 1);
+
+    char buffer[7];
+    int readed = read(fd[0], buffer, 6);
+    buffer[6] = 0;
+    ASSERT_STREQ("sa\npb\n", buffer);
+    ASSERT_EQ(6, result);
 }
