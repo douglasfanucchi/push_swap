eb050e9bde4a29ec9dc530521848f7fec719def7
Douglas Fanucchi <douglasfanucchi@gmail.com>
2026-06-25 18:52:44 -0300

    refact(ft_normalize_int_arr): it changes argument to be a pointer to array instead of a copy

--- ft_parser.c
+++ ft_parser.c
@@ -6,21 +6,21 @@
 /*   By: dode-lim <dode-lim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/23 19:06:05 by dode-lim          #+#    #+#             */
-/*   Updated: 2026/06/25 18:39:22 by dode-lim         ###   ########.fr       */
+/*   Updated: 2026/06/25 18:49:30 by dode-lim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-t_arr	ft_normalize_int_arr(t_arr arr)
+t_arr	ft_normalize_int_arr(t_arr *arr)
 {
 	int		i;
 	t_arr	result;
 	t_arr	pairs;
 
-	result.len = arr.len;
+	result.len = arr->len;
 	result.elements = malloc(sizeof(int) * result.len);
-	ft_memmove(result.elements, arr.elements, sizeof(int) * (result.len));
+	ft_memmove(result.elements, arr->elements, sizeof(int) * (result.len));
 	pairs.len = result.len;
 	pairs.elements = malloc(sizeof(int *) * (result.len));
 	i = -1;
--- tests/ft_parser_test.cc
+++ tests/ft_parser_test.cc
@@ -10,7 +10,7 @@ TEST(ft_parser, itShouldNormalizeAnIntegerArray) {
 	arr.elements = malloc(sizeof(int) * 7);
 	ft_memmove(arr.elements, (int[]){1, 2, 3, 42, -100, 2147483647, -2147483648}, 7 * sizeof(int));
 
-	t_arr normalized = ft_normalize_int_arr(arr);
+	t_arr normalized = ft_normalize_int_arr(&arr);
 
 	ASSERT_EQ(2, ((int *)normalized.elements)[0]);
 	ASSERT_EQ(3, ((int *)normalized.elements)[1]);
