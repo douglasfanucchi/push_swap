8df3dbbd5c67ea38ef9e3f847d534838dfea360a
Douglas Fanucchi <douglasfanucchi@gmail.com>
2026-06-25 16:52:27 -0300

    tests(ft_quicksort): it should sort an array of pairs based on pair's second value

--- tests/ft_quicksort_test.cc
+++ tests/ft_quicksort_test.cc
@@ -23,3 +23,30 @@ TEST(ft_quicksort, itShouldSortAnIntegerArray) {
 
 	free(arr.elements);
 }
+
+TEST(ft_quicksort, itShouldSortAnArrayOfPairsBasedOnTheSecondValueOfThePair) {
+	int numbers[] = {1, 2, 3, 42, -100, 2147483647, -2147483648};
+	t_arr arr;
+	arr.len = 7;
+	arr.elements = malloc(sizeof(int *) * 7);
+	int	**pairs = (int **)arr.elements;
+	for(int i = 0; i < 7; i++) {
+		pairs[i] = (int *)malloc(sizeof(int) * 2);
+		pairs[i][0] = i;
+		pairs[i][1] = numbers[i];
+	}
+
+	ft_quicksort(arr, sizeof(int *), ft_less_than_pair, ft_swap_pair);
+
+	ASSERT_EQ(-2147483648, pairs[0][1]);
+	ASSERT_EQ(-100, pairs[1][1]);
+	ASSERT_EQ(1, pairs[2][1]);
+	ASSERT_EQ(2, pairs[3][1]);
+	ASSERT_EQ(3, pairs[4][1]);
+	ASSERT_EQ(42, pairs[5][1]);
+	ASSERT_EQ(2147483647, pairs[6][1]);
+
+	for(int i = 0; i < 7; i++)
+		free(((int **)arr.elements)[i]);
+	free(arr.elements);
+}
