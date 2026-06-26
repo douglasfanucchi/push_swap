0c4a905b6d6846c207d1a2733df26e91e856464a
Douglas Fanucchi <douglasfanucchi@gmail.com>
2026-06-26 14:36:30 -0300

    tests(ft_input_validator): it should return false when input has duplicated numbers

--- tests/ft_input_validator_test.cc
+++ tests/ft_input_validator_test.cc
@@ -17,3 +17,17 @@ TEST(ft_input_validator, itShouldReturnTrueWhenInputIsJustAValidSetOfNumbers) {
 
     ASSERT_TRUE(result);
 }
+
+TEST(ft_input_validator, itShouldReturnFalseWhenInputHasDuplicatedNumber) {
+    const char *argv[] = {
+        "push_swap",
+        "1",
+        "3 100 -10",
+        "-2147483648 2147483647 1",
+        NULL
+    };
+
+    t_bool result = ft_input_validator(argv + 1);
+
+    ASSERT_FALSE(result);
+}
