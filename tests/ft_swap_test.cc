cf84d47d51912f148fce99c5a04a87c1e554bbbe
Douglas Fanucchi <douglasfanucchi@gmail.com>
2026-06-26 17:48:37 -0300

    tests(ft_swap): it should swap elements from both stacks

--- tests/ft_swap_test.cc
+++ tests/ft_swap_test.cc
@@ -6,6 +6,7 @@ extern "C" {
 
 TEST(ft_swap, itShouldSwapElementFromStackA) {
     t_stack a;
+    ft_stack_init(&a);
     ft_stack_push(&a, 42);
     ft_stack_push(&a, 43);
 
@@ -27,6 +28,28 @@ TEST(ft_swap, itShouldSwapElementFromStackB) {
     t_operations operation = ft_swap_b(NULL, &b);
 
     ASSERT_EQ(sb, operation);
+    ASSERT_EQ(42, ft_stack_peek(&b));
+    ft_stack_pop(&b);
+    ASSERT_EQ(43, ft_stack_peek(&b));
+    ft_stack_pop(&b);
+}
+
+TEST(ft_swap, itShouldSwapElementFromBothStacks) {
+    t_stack a, b;
+    ft_stack_init(&a);
+    ft_stack_init(&b);
+    ft_stack_push(&a, 42);
+    ft_stack_push(&a, 43);
+    ft_stack_push(&b, 42);
+    ft_stack_push(&b, 43);
+
+    t_operations operation = ft_swap_ss(&a, &b);
+
+    ASSERT_EQ(ss, operation);
+    ASSERT_EQ(42, ft_stack_peek(&a));
+    ft_stack_pop(&a);
+    ASSERT_EQ(43, ft_stack_peek(&a));
+    ft_stack_pop(&a);
     ASSERT_EQ(42, ft_stack_peek(&b));
     ft_stack_pop(&b);
     ASSERT_EQ(43, ft_stack_peek(&b));
