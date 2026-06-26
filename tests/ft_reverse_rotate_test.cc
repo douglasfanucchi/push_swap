e0f427590add25438ebeb289191cde0ffabb63b4
Douglas Fanucchi <douglasfanucchi@gmail.com>
2026-06-26 18:25:59 -0300

    tests(ft_reverse_rotate): it should reverse rotate stack b

--- tests/ft_reverse_rotate_test.cc
+++ tests/ft_reverse_rotate_test.cc
@@ -21,3 +21,21 @@ TEST(ft_reverse_rotate, itShouldReverseRotateStackA) {
     ASSERT_EQ(42, ft_stack_peek(&a));
     ft_stack_pop(&a);
 }
+
+TEST(ft_reverse_rotate, itShouldReverseRotateStackB) {
+    t_stack b;
+    ft_stack_init(&b);
+    ft_stack_push(&b, 41);
+    ft_stack_push(&b, 42);
+    ft_stack_push(&b, 43);
+
+    t_operations operation = ft_reverse_rotate_b(NULL, &b);
+
+    ASSERT_EQ(rrb, operation);
+    ASSERT_EQ(41, ft_stack_peek(&b));
+    ft_stack_pop(&b);
+    ASSERT_EQ(43, ft_stack_peek(&b));
+    ft_stack_pop(&b);
+    ASSERT_EQ(42, ft_stack_peek(&b));
+    ft_stack_pop(&b);
+}
