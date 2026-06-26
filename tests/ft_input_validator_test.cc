84323fb822bacc79d869e7b654f10f894d0446d0
Douglas Fanucchi <douglasfanucchi@gmail.com>
2026-06-26 14:33:57 -0300

    tests(ft_input_validator): it should return true when input is just a valid set of numbers

---
+++ tests/ft_input_validator_test.cc
@@ -0,0 +1,19 @@
+#include <gtest/gtest.h>
+
+extern "C" {
+    #include "push_swap.h"
+}
+
+TEST(ft_input_validator, itShouldReturnTrueWhenInputIsJustAValidSetOfNumbers) {
+    const char *argv[] = {
+        "push_swap",
+        "1",
+        "3 100 -10",
+        "-2147483648 2147483647",
+        NULL
+    };
+
+    t_bool result = ft_input_validator(argv + 1);
+
+    ASSERT_TRUE(result);
+}
