ee4f80d66cf1e1c945726605f32dab4a66e22325
Douglas Fanucchi <douglasfanucchi@gmail.com>
2026-06-26 14:17:24 -0300

    tests(ft_parser): it should create a stack with normalized numbers input

--- tests/ft_parser_test.cc
+++ tests/ft_parser_test.cc
@@ -67,3 +67,24 @@ TEST(ft_parser, itShouldDefineStrategyAsComplexWhenItIsSpecified) {
 
 	ASSERT_EQ(complex, program.strategy);
 }
+
+TEST(ft_parser, itShouldCreateAStackWithNormalizedNumbersInput) {
+	t_stack	stack;
+	const char *argv[] = {
+		"push_swap",
+		"1239812",
+		"-129312",
+		"3",
+		NULL
+	};
+
+	stack = ft_parse_numbers(argv + 1);
+
+	ASSERT_EQ(2, ft_stack_peek(&stack));
+	ft_stack_rotate(&stack);
+	ASSERT_EQ(0, ft_stack_peek(&stack));
+	ft_stack_rotate(&stack);
+	ASSERT_EQ(1, ft_stack_peek(&stack));
+
+	ft_stack_clear(&stack);
+}
