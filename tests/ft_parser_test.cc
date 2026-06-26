6e7978c42a182a910004ed0e4243ca29d1127006
Douglas Fanucchi <douglasfanucchi@gmail.com>
2026-06-26 14:07:37 -0300

    tests(ft_parser): it should define strategy as adaptative and bench as true when both are specified

--- tests/ft_parser_test.cc
+++ tests/ft_parser_test.cc
@@ -37,3 +37,19 @@ TEST(ft_parser, itShouldDefineStrategyAsAdaptativeAndBenchAsFalseWhenNotSpecifyi
 	ASSERT_EQ(adaptative, program.strategy);
 	ASSERT_FALSE(program.bench);
 }
+
+TEST(ft_parser, itShouldDefineStrategyAsAdaptativeAndBenchAsTrueWhenNotSpecifyingBothValues) {
+	t_program program;
+	const char *argv[] = {
+		"push_swap",
+		"--bench",
+		"--adaptative",
+		"1",
+		NULL
+	};
+
+	ft_parse_flags(&program, argv + 1);
+
+	ASSERT_EQ(adaptative, program.strategy);
+	ASSERT_TRUE(program.bench);
+}
