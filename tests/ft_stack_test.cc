5bd5301e18475c312e35a9c9caf5987e354bf7ac
Douglas Fanucchi <douglasfanucchi@gmail.com>
2026-06-30 17:03:41 -0300

    tests(ft_stack): it should return an index of the first number that satisfies a callback

--- tests/ft_stack_test.cc
+++ tests/ft_stack_test.cc
@@ -65,3 +65,25 @@ TEST(ft_stack, itShouldReturnANumberGivenItsPosition) {
 
 	ASSERT_EQ(43, result);
 }
+
+static t_bool	ft_stack_number_lt(int a, int b)
+{
+	return (b < a);
+}
+
+TEST(ft_stack, itShouldreturnAnIndexOfTheFirstNumberThatSatisfiesACallback) {
+	t_stack	stack;
+	t_dlist node_1, node_2;
+	stack.size = 2;
+	stack.head = &node_1;
+	node_1.n = 43;
+	node_1.next = &node_2;
+	node_1.prev = &node_2;
+	node_2.prev = &node_1;
+	node_2.next = &node_1;
+	node_2.n = 42;
+
+	int	result = ft_stack_find(&stack, 43, ft_stack_number_lt);
+
+	ASSERT_EQ(1, result);
+}
