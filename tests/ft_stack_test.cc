1f892ede5f4c3d452b6697eea434e15dd592d6f1
Douglas Fanucchi <douglasfanucchi@gmail.com>
2026-06-30 17:06:03 -0300

    tests(ft_stack): it should return negative one when no number satisfies a callback

--- tests/ft_stack_test.cc
+++ tests/ft_stack_test.cc
@@ -87,3 +87,20 @@ TEST(ft_stack, itShouldreturnAnIndexOfTheFirstNumberThatSatisfiesACallback) {
 
 	ASSERT_EQ(1, result);
 }
+
+TEST(ft_stack, itShouldreturnNegativeOneWhenNoNumberSatisfiesACallback) {
+	t_stack	stack;
+	t_dlist node_1, node_2;
+	stack.size = 2;
+	stack.head = &node_1;
+	node_1.n = 43;
+	node_1.next = &node_2;
+	node_1.prev = &node_2;
+	node_2.prev = &node_1;
+	node_2.next = &node_1;
+	node_2.n = 42;
+
+	int	result = ft_stack_find(&stack, 42, ft_stack_number_lt);
+
+	ASSERT_EQ(-1, result);
+}
