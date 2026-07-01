84b68d182581d835572636364c644741269d0975
Douglas Fanucchi <douglasfanucchi@gmail.com>
2026-07-01 14:59:15 -0300

    tests(ft_execute_algorithm): it shoudl return a n√n algorithm when disorder is between 0.2 and 0.5 when strategy adaptative

--- tests/ft_execute_algorithm_test.cc
+++ tests/ft_execute_algorithm_test.cc
@@ -22,3 +22,28 @@ TEST(ft_execute_algorithm, itShouldReturnA_nlogn_AlgorithmWhenDisorderIsOneAndSt
 	ft_state_clear(&result);
 	ft_stack_clear(&a);
 }
+
+TEST(ft_execute_algorithm, itShouldReturnA_nsqrtn_AlgorithmWhenDisorderIsBetween_0_2_And_0_5_StrategyAdaptative) {
+	t_stack a;
+	ft_stack_init(&a);
+	ft_stack_push(&a, 2);
+	ft_stack_push(&a, 3);
+	ft_stack_push(&a, 4);
+	ft_stack_push(&a, 1);
+	ft_stack_push(&a, 0);
+
+	t_state result = ft_execute_algorithm(adaptative, 0.30, &a);
+
+	ASSERT_EQ(5, a.size);
+	ASSERT_EQ(0, ft_stack_get_nth_number(&a, 1));
+	ASSERT_EQ(1, ft_stack_get_nth_number(&a, 2));
+	ASSERT_EQ(4, ft_stack_get_nth_number(&a, 3));
+	ASSERT_EQ(3, ft_stack_get_nth_number(&a, 4));
+	ASSERT_EQ(2, ft_stack_get_nth_number(&a, 5));
+	ASSERT_STREQ("O(n√n)", result.complexity);
+	ASSERT_NE(nullptr, result.ops);
+	ASSERT_LT(0, result.total_ops);
+
+	ft_state_clear(&result);
+	ft_stack_clear(&a);
+}
