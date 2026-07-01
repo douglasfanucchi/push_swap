7a03bf0681a1cbbf0151c004c52e8f25f8b5eb9c
Douglas Fanucchi <douglasfanucchi@gmail.com>
2026-07-01 18:37:07 -0300

    tests(push_swap): it should print program state

--- tests/push_swap_test.cc
+++ tests/push_swap_test.cc
@@ -2,6 +2,8 @@
 
 extern "C" {
 	#include "push_swap.h"
+	#include <fcntl.h>
+    #include <unistd.h>
 }
 
 static t_operations	ft_test_swap_a(t_stack *stack_a, t_stack *stack_b)
@@ -295,4 +297,57 @@ TEST(push_swap, itShouldReturnTrueOnOperationSuccess) {
 	ASSERT_TRUE(result);
 
 	ft_lstclear(&state.ops, free);
+}
+
+TEST(push_swap, itShouldPrintProgramState) {
+	t_program	program;
+	int sqrt_symbol_bytes = 3;
+	program.state.total_ops = 13;
+	program.state.complexity = ft_strdup("O(n√n)");
+	program.state.count_ops[sa] = 0;
+	program.state.count_ops[sb] = 0;
+	program.state.count_ops[ss] = 0;
+	program.state.count_ops[pa] = 5;
+	program.state.count_ops[pb] = 5;
+	program.state.count_ops[ra] = 2;
+	program.state.count_ops[rb] = 1;
+	program.state.count_ops[rr] = 0;
+	program.state.count_ops[rra] = 0;
+	program.state.count_ops[rrb] = 0;
+	program.state.count_ops[rrr] = 0;
+	program.disorder = 0.4;
+	program.strategy = adaptative;
+	int stderr = dup(2), fd[2];
+    int piped = pipe2(fd, O_NONBLOCK);
+    dup2(fd[1], 2);
+
+    ft_program_print(&program);
+    dup2(stderr, 2);
+
+	char buffer[20000];
+	char	*line;
+	int readed = read(fd[0], buffer, 20000);
+	ASSERT_NE(0, readed);
+
+	line = ft_substr(buffer, 0, 26);
+	ASSERT_STREQ("[bench] disorder:  40.00%\n", line);
+	free(line);
+
+	line = ft_substr(buffer, 26, 38 + sqrt_symbol_bytes);
+	ASSERT_STREQ("[bench] strategy:  Adaptative / O(n√n)\n", line);
+	free(line);
+
+	line = ft_substr(buffer, 64 + sqrt_symbol_bytes, 23);
+	ASSERT_STREQ("[bench] total_ops:  13\n", line);
+	free(line);
+
+	line = ft_substr(buffer, 87 + sqrt_symbol_bytes, 47);
+	ASSERT_STREQ("[bench] sa:  0  sb:  0  ss:  0  pa:  5  pb:  5\n", line);
+	free(line);
+
+	line = ft_substr(buffer, 134 + sqrt_symbol_bytes, 58);
+	ASSERT_STREQ("[bench] ra:  2  rb:  1  rr:  0  rra:  0  rrb:  0  rrr:  0\n", line);
+	free(line);
+	
+	free(program.state.complexity);
 }
