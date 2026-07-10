b20abba32dfb652b6f2c45d74ce7d20149e49d4f
Douglas Fanucchi <douglasfanucchi@gmail.com>
2026-07-10 18:26:51 -0300

    tests(ft_move_ith_to): it replicates tests to stack B

--- tests/ft_algo_utils_test.cc
+++ tests/ft_algo_utils_test.cc
@@ -72,3 +72,73 @@ TEST(ft_move_ith_to, itShouldMoveAnElementFromTopToTheEndByRotate) {
 	ft_state_clear(&state);
 	ft_stack_clear(&a);
 }
+
+TEST(ft_move_ith_to, itShouldMoveAnElementThatIsInTheFirstHalfOfTheStackBToTopByRotate) {
+	t_stack	b;
+	t_state	state;
+	t_move move;
+	move.from = 1;
+	move.to = 0;
+	ft_state_init(&state);
+	ft_stack_init(&b);
+	ft_stack_push(&b, 41);
+	ft_stack_push(&b, 42);
+	ft_stack_push(&b, 43);
+	ft_stack_push(&b, 44);
+
+	ft_move_ith_to(move, &state, &b, rb);
+
+	ASSERT_EQ(1, state.total_ops);
+	ASSERT_STREQ("rb", (char *)state.ops->content);
+	ASSERT_EQ(43, ft_stack_peek(&b));
+
+	ft_state_clear(&state);
+	ft_stack_clear(&b);
+}
+
+TEST(ft_move_ith_to, itShouldMoveAnElementThatIsInTheSecondHalfOfTheStackBToTheTopByReverseRotate) {
+	t_stack	b;
+	t_state	state;
+	t_move move;
+	move.from = 3;
+	move.to = 0;
+	ft_state_init(&state);
+	ft_stack_init(&b);
+	ft_stack_push(&b, 41);
+	ft_stack_push(&b, 42);
+	ft_stack_push(&b, 43);
+	ft_stack_push(&b, 44);
+
+	ft_move_ith_to(move, &state, &b, rb);
+
+	ASSERT_EQ(1, state.total_ops);
+	ASSERT_STREQ("rrb", (char *)state.ops->content);
+	ASSERT_EQ(41, ft_stack_peek(&b));
+
+	ft_state_clear(&state);
+	ft_stack_clear(&b);
+}
+
+TEST(ft_move_ith_to, itShouldMoveAnElementFromTopOfTheStackBToTheEndByRotate) {
+	t_stack	b;
+	t_state	state;
+	t_move move;
+	move.from = 0;
+	move.to = 3;
+	ft_state_init(&state);
+	ft_stack_init(&b);
+	ft_stack_push(&b, 41);
+	ft_stack_push(&b, 42);
+	ft_stack_push(&b, 43);
+	ft_stack_push(&b, 44);
+
+	ft_move_ith_to(move, &state, &b, rb);
+
+	ASSERT_EQ(1, state.total_ops);
+	ASSERT_STREQ("rb", (char *)state.ops->content);
+	ASSERT_EQ(44, ft_stack_get_nth_number(&b, 4));
+
+	ft_state_clear(&state);
+	ft_stack_clear(&b);
+}
+
