979b3dbb2b86bbc9aa7e84b622eec748478028c8
Douglas Fanucchi <douglasfanucchi@gmail.com>
2026-07-10 15:43:57 -0300

    tests(ft_stack_find_offset): it should find first occurence of an element limited by substack size

--- tests/ft_stack_operations_2_test.cc
+++ tests/ft_stack_operations_2_test.cc
@@ -59,3 +59,26 @@ TEST(ft_stack_find_offset, itShouldFindFirstOccurenceOfAnElementLimitedBySubstac
 
 	ASSERT_EQ(0, result);
 }
+
+TEST(ft_stack_find_offset, itShouldFindFirstOccurenceOfAnElementLimitedBySubstackSize) {
+	t_dlist	node_1, node_2, node_3;
+	t_stack	stack;
+	t_substack substack;
+	stack.size = 3;
+	stack.head = &node_1;
+	node_1.next = &node_2;
+	node_1.prev = &node_3;
+	node_1.n = 42;
+	node_2.prev = &node_1;
+	node_2.next = &node_3;
+	node_2.n = 43;
+	node_3.prev = &node_2;
+	node_3.next = &node_1;
+	node_3.n = 44;
+	substack.start = 0;
+	substack.size = stack.size - 1;
+
+	int result = ft_stack_find_offset(&stack, substack, 45, ft_int_lt);
+
+	ASSERT_EQ(0, result);
+}
