25454b0bdd58956e2fc309f88e5626e214a813bd
Douglas Fanucchi <douglasfanucchi@gmail.com>
2026-07-10 18:18:48 -0300

    feat(ft_move_ith_to): it should move an element from top to the end by rate

--- tests/ft_algo_utils_test.cc
+++ tests/ft_algo_utils_test.cc
@@ -49,3 +49,26 @@ TEST(ft_move_ith_to, itShouldMoveAnElementThatIsInTheSecondHalfOfTheStackAToTheT
 	ft_state_clear(&state);
 	ft_stack_clear(&a);
 }
+
+TEST(ft_move_ith_to, itShouldMoveAnElementFromTopToTheEndByRotate) {
+	t_stack	a;
+	t_state	state;
+	t_move move;
+	move.from = 0;
+	move.to = 3;
+	ft_state_init(&state);
+	ft_stack_init(&a);
+	ft_stack_push(&a, 41);
+	ft_stack_push(&a, 42);
+	ft_stack_push(&a, 43);
+	ft_stack_push(&a, 44);
+
+	ft_move_ith_to(move, &state, &a, ra);
+
+	ASSERT_EQ(1, state.total_ops);
+	ASSERT_STREQ("ra", (char *)state.ops->content);
+	ASSERT_EQ(44, ft_stack_get_nth_number(&a, 4));
+
+	ft_state_clear(&state);
+	ft_stack_clear(&a);
+}
