ef7ab238f4149a5814b5955dbae918d30c2a9239
Douglas Fanucchi <douglasfanucchi@gmail.com>
2026-07-10 15:39:28 -0300

    tests(ft_stack_find_offset): it should find first occurence of an element in the substack ignoring elements outside of it

--- tests/ft_stack_operations_2_test.cc
+++ tests/ft_stack_operations_2_test.cc
@@ -9,6 +9,11 @@ static t_bool	ft_int_eq(int a, int b)
 	return (a == b);
 }
 
+static t_bool ft_int_lt(int a, int b)
+{
+	return (b < a);
+}
+
 TEST(ft_stack_find_offset, itShouldSearchForAnElementInTheWholeStackThatSatisfiesCallback) {
 	t_dlist	node_1, node_2, node_3;
 	t_stack	stack;
@@ -30,4 +35,27 @@ TEST(ft_stack_find_offset, itShouldSearchForAnElementInTheWholeStackThatSatisfie
 	int result = ft_stack_find_offset(&stack, substack, 44, ft_int_eq);
 
 	ASSERT_EQ(2, result);
+}
+
+TEST(ft_stack_find_offset, itShouldFindFirstOccurenceOfAnElementLimitedBySubstackStart) {
+	t_dlist	node_1, node_2, node_3;
+	t_stack	stack;
+	t_substack substack;
+	stack.size = 3;
+	stack.head = &node_1;
+	node_1.next = &node_2;
+	node_1.prev = &node_3;
+	node_1.n = 42;
+	node_2.prev = &node_1;
+	node_2.next = &node_3;
+	node_2.n = 43;
+	node_3.prev = &node_2;
+	node_3.next = &node_1;
+	node_3.n = 44;
+	substack.start = 1;
+	substack.size = stack.size - 1;
+
+	int result = ft_stack_find_offset(&stack, substack, 45, ft_int_lt);
+
+	ASSERT_EQ(0, result);
 }
