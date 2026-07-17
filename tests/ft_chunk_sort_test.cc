2c748b2a9642c86dc02c1a72de0658b3d9ee3b81
Douglas Fanucchi <douglasfanucchi@gmail.com>
2026-07-17 15:53:31 -0300

    tests(ft_reverse_insertion_sort_substack): it should do nothing when stack is already reverse sorted

--- tests/ft_chunk_sort_test.cc
+++ tests/ft_chunk_sort_test.cc
@@ -143,3 +143,26 @@ TEST(ft_reverse_insertion_sort_substack, itShouldSortInDescendingOrderSubstackWi
     ft_stack_clear(&b);
     ft_state_clear(&state);
 }
+
+TEST(ft_reverse_insertion_sort_substack, itShouldDoNothingWhenStackIsAlreadyReverseSorted) {
+    t_stack a;
+    t_stack b;
+    t_state state;
+    ft_stack_init(&a);
+    ft_stack_init(&b);
+    ft_stack_push(&b, 0);
+    ft_stack_push(&b, 1);
+    ft_state_init(&state);
+
+    ft_reverse_insertion_sort_substack(&a, &b, &state, 2);
+
+    ASSERT_EQ(0, state.total_ops);
+    ASSERT_EQ(2, b.size);
+    ASSERT_EQ(0, a.size);
+    ASSERT_EQ(1, ft_stack_peek(&b));
+    ft_stack_rotate(&b);
+    ASSERT_EQ(0, ft_stack_peek(&b));
+    ft_stack_clear(&a);
+    ft_stack_clear(&b);
+    ft_state_clear(&state);
+}
